7d1f963312bab26575d915e261e7d819d9987194
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-19 14:40:07 +0000

    docs(`InferSettings`): update deprecation notice for InferSettings to recommend using InferSettings from @typedly/configurable-data

--- src/lib/infer-settings.type.ts
+++ src/lib/infer-settings.type.ts
@@ -2,6 +2,8 @@
 import { DataAdapterShape } from "@typedly/data-adapter";
 import { AdaptableDataShape } from "./adaptable.data.shape";
 /**
+ * @deprecated This utility is redundant and should not be used. In favor of just using `InferSettings` from `@typedly/configurable-data` directly.
+ * @see InferSettings from `@typedly/configurable-data` for a more robust and comprehensive type inference utility.
  * @description Infers the settings type from a data adapter, or adaptable data shape.
  * @export
  * @template T 
