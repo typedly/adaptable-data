c3752362285f5487bd98d6cf9b9853a9b683f214
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 16:10:17 +0000

    refactor: remove unused InferAsyncOf type and simplify async flag definition in AdaptableConfigurableDataShape interface

--- src/lib/adaptable.configurable.data.shape.ts
+++ src/lib/adaptable.configurable.data.shape.ts
@@ -2,7 +2,6 @@
 import type { ConfigurableDataAdapterShape } from "@typedly/data-adapter";
 import type { DataSettings } from "@typedly/data";
 // Type.
-import type { InferAsyncOf } from "@typedly/data";
 import type { AdaptableConfigurableBase } from "./adaptable.configurable.base";
 /**
  * @description The shape of a data type with optional adapter.
@@ -11,12 +10,12 @@ import type { AdaptableConfigurableBase } from "./adaptable.configurable.base";
  * @template {ConfigurableDataAdapterShape<C, T, R> | undefined} A The adapter type, which can be undefined for more flexible inference.
  * @template {DataSettings<R> | undefined} C The settings type.
  * @template T The value type.
- * @template {boolean} [R=InferAsyncOf<[C, A]>] The async flag, inferred from C or `A` if possible.
+ * @template {boolean} R The async flag.
  * @extends {AdaptableConfigurableBase<A, C, T, R>}
  */
 export interface AdaptableConfigurableDataShape<
   A extends ConfigurableDataAdapterShape<C, T, R> | undefined,
   C extends DataSettings<R> | undefined,
   T,
-  R extends boolean = InferAsyncOf<[C, A]>
+  R extends boolean
 > extends AdaptableConfigurableBase<A, C, T, R> {}
