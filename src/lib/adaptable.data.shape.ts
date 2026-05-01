42a580488479c7b3247e4033b78e804142affe2f
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 16:10:33 +0000

    refactor: simplify async flag definition in AdaptableDataShape interface

--- src/lib/adaptable.data.shape.ts
+++ src/lib/adaptable.data.shape.ts
@@ -1,19 +1,19 @@
 // Interface.
 import type { Adaptable } from "@typedly/data-traits";
 import type { DataAdapterShape } from "@typedly/data-adapter";
-import type { DataShape, InferAsync } from "@typedly/data";
+import type { DataShape } from "@typedly/data";
 /**
  * @description The shape of a data type with optional adapter.
  * @export
  * @interface AdaptableDataShape
  * @template {DataAdapterShape<T, R> | undefined} A The adapter type, which can be undefined for more flexible inference.
- * @template T The value type.
- * @template {boolean} [R=InferAsync<A>] The async flag, inferred from `A` if possible.
+ * @template T The value type, which can be inferred from `A` if possible.
+ * @template {boolean} R The async flag, which can be inferred from `A` if possible.
  * @extends {Adaptable<A>}
  * @extends {DataShape<T, R>}
  */
 export interface AdaptableDataShape<
   A extends DataAdapterShape<T, R> | undefined,
   T,
-  R extends boolean = InferAsync<A>
+  R extends boolean
 > extends Adaptable<A>, DataShape<T, R> {}
